ada411d0ecdcb4130222ab45dcb14e9d22f39b00
Paulo Costa <me@paulo.costa.nom.br>
2022-07-18 03:33:19 -0300

    Fix typo: listen_reverser_base_parser -> listen_reversed_base_parser

--- adbproxy/main.py
+++ adbproxy/main.py
@@ -15,10 +15,10 @@ async def main():
     hostinfo_parser = argparse.ArgumentParser(add_help=False)
     hostinfo_parser.add_argument("--host-adb-server", dest="host_adb_addr", type=sockaddr, default=("localhost", 5037), help="Socket address of ADB server away from the device")
 
-    listen_reverser_base_parser = argparse.ArgumentParser(add_help=False, parents=[hostinfo_parser, ssh_jump_parser])
-    listen_reverser_base_parser.add_argument("listen_address", type=ssh_config, nargs='?', default={"host": "0.0.0.0"}, help="Server address where the reverse SSH server will be bound")
-    listen_reverser_base_parser.add_argument("--upnp", action='store_true', help="Uses UPNP to setup the Internet Gateway to receive incoming connections")
-    listen_reverser_base_parser.add_argument("--ngrok", dest="ssh_tunnels", action="append_const", const={"host": "tunnel.us.ngrok.com", "port": 22, "known_hosts": None, "password": ''}, help="Uses Ngrok as a Gateway")
+    listen_reverse_base_parser = argparse.ArgumentParser(add_help=False, parents=[hostinfo_parser, ssh_jump_parser])
+    listen_reverse_base_parser.add_argument("listen_address", type=ssh_config, nargs='?', default={"host": "0.0.0.0"}, help="Server address where the reverse SSH server will be bound")
+    listen_reverse_base_parser.add_argument("--upnp", action='store_true', help="Uses UPNP to setup the Internet Gateway to receive incoming connections")
+    listen_reverse_base_parser.add_argument("--ngrok", dest="ssh_tunnels", action="append_const", const={"host": "tunnel.us.ngrok.com", "port": 22, "known_hosts": None, "password": ''}, help="Uses Ngrok as a Gateway")
 
     subparsers = parser.add_subparsers(help='commands', dest='cmd')
     subparsers.required = True
@@ -30,10 +30,10 @@ async def main():
     parser_connect_reverse.add_argument("server_address", type=ssh_config, nargs='?', default={}, help="Address that the remote ADB-Proxy is listening on")
     parser_connect_reverse.set_defaults(func=connect_reverse)
 
-    parser_listen_reverse = subparsers.add_parser('listen-reverse', parents=[listen_reverser_base_parser], help='Awaits reverse connections from devices')
+    parser_listen_reverse = subparsers.add_parser('listen-reverse', parents=[listen_reverse_base_parser], help='Awaits reverse connections from devices')
     parser_listen_reverse.set_defaults(func=listen_reverse)
 
-    parser_df = subparsers.add_parser('devicefarm', parents=[listen_reverser_base_parser], help='Awaits connections from DeviceFarm')
+    parser_df = subparsers.add_parser('devicefarm', parents=[listen_reverse_base_parser], help='Awaits connections from DeviceFarm')
     parser_df.add_argument("--project", dest='project_name', default="Remote Debug", help="Project Name")
     parser_df_group = parser_df.add_mutually_exclusive_group()
     parser_df_group.add_argument("--device-pool", dest='device_pool', default="Default Pool", help="Device Pool")
